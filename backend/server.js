06ea2f4a6104b517b0af1292b261711d9f947dee
veeresh <veerreshr@gmail.com>
2020-12-04 01:28:15 +0530

    basic backend with mongodb setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { config } from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+config();
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
